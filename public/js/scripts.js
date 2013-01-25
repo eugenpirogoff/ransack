ae985ec2671312908ae3cfa1ab838bcbb75b9424
Eugen Pirogoff <eugenpirogoff@me.com>
2013-01-25 18:29:50 +0100

    result sidebar bug fix

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -31,26 +31,18 @@ $(window).resize(function(){
 //result slider from left 
 $(document).ready(function() {
     $("#sidebar").bind("mouseenter",function(){
-      $("#sidebar").animate({"left": "+=230px"}, "slow");
-      $('.mymap').animate({"margin-left": "+=230px"}, "slow").animate({"width": "-=230px"}, "slow").dequeue();
+      $("#sidebar").animate({"left": "+=230px"}, "fast");
+      $('.mymap').animate({"margin-left": "+=230px"}, "fast").animate({"width": "-=230px"}, "fast").dequeue();
     }).bind("mouseleave",function(){
       $('.mymap').animate({"width": "+=230px"}, "fast").animate({"margin-left":"-=230px"},"fast").dequeue();
       $("#sidebar").animate({"left": "-=230px"}, "fast");
     });
   });
 
-
-/*var showtimes=1;
-function showonce(){
-	if(showtimes>0){
-   $("#sidebar").bind("mouseenter",function(){
-      $("#sidebar").animate({"left": "+=230px"}, "slow");
-      $('.mymap').animate({"margin-left": "+=230px"}, "slow").animate({"width": "-=230px"}, "slow").dequeue();
-    }).bind("mouseleave",function(){
-      $('.mymap').animate({"width": "+=230px"}, "fast").animate({"margin-left":"-=230px"},"fast").dequeue();
-      $("#sidebar").animate({"left": "-=230px"}, "fast");
+$(document).ready(function() {
+    $(window).bind("mouseleave,mouseout",function(){
+      $('.mymap').animate({"width": $(window).innerWidth()-30}, "fast").animate({"margin-left":"=230px"},"fast").dequeue();
+      $("#sidebar").animate({"left": "-230px"}, "fast");
     });
- 
-	showtimes=0;
-	} 
-}*/
+  });
+
